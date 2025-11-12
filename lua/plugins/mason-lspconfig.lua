ad64670f1f4f1129e62a2aa8db8c75a28f0ffa56
Lucas Ilea <lucas1.ilea@gmail.com>
2025-11-12 17:20:00 -0500

    ensure installed copilot and lua_ls servers

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,38 +1,42 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
+	{
+		"mason-org/mason-lspconfig.nvim",
 
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
 
-    opts = {
-      automatic_enable = true,
-    },
+		opts = {
+			automatic_enable = true,
+			ensure_installed = {
+				"lua_ls",
+				"copilot",
+			},
+		},
 
-    config = function(_, opts)
-      local mason_lsp = require("mason-lspconfig")
-      local blink_caps = require("blink.cmp").get_lsp_capabilities()
+		config = function(_, opts)
+			local mason_lsp = require("mason-lspconfig")
+			local blink_caps = require("blink.cmp").get_lsp_capabilities()
 
-      mason_lsp.setup(opts)
+			mason_lsp.setup(opts)
 
-      for _, server in ipairs(mason_lsp.get_installed_servers()) do
-        vim.lsp.config(server, {
-          capabilities = blink_caps,
-          on_attach = function(_, bufnr)
-            local map = function(mode, lhs, rhs)
-              vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true })
-            end
-            map("n", "K", vim.lsp.buf.hover)
-            map("n", "gd", vim.lsp.buf.definition)
-            map("n", "gr", vim.lsp.buf.references)
-            map("n", "<leader>rn", vim.lsp.buf.rename)
-            map("n", "<leader>ca", vim.lsp.buf.code_action)
-          end,
-        })
-        vim.lsp.enable(server)
-      end
-    end
-  }
+			for _, server in ipairs(mason_lsp.get_installed_servers()) do
+				vim.lsp.config(server, {
+					capabilities = blink_caps,
+					on_attach = function(_, bufnr)
+						local map = function(mode, lhs, rhs)
+							vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true })
+						end
+						map("n", "K", vim.lsp.buf.hover)
+						map("n", "gd", vim.lsp.buf.definition)
+						map("n", "gr", vim.lsp.buf.references)
+						map("n", "<leader>rn", vim.lsp.buf.rename)
+						map("n", "<leader>ca", vim.lsp.buf.code_action)
+					end,
+				})
+				vim.lsp.enable(server)
+			end
+		end,
+	},
 }
